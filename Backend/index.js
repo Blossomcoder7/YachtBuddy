06257de596ca1144b14aa7a02679fefbbacc16bc
root <root@ubuntu-cloudpanel.localhost>
2023-11-02 13:19:19 +0000

    paypal

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,11 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
+<<<<<<< HEAD
   origin: ['http://localhost:3000', 'https://www.sandbox.paypal.com'],
+=======
+  origin: ['https://theyachtbuddy.com','theyachtbuddy.com'],
+>>>>>>> 280b0a7 (forget password)
   credentials: true
 };
 
